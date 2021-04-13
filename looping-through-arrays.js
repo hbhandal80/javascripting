3c2466feca5e76ce448e91bb4272eac45879108e
Hervinder Bhandal <hbhandal80@hotmail.com>
2021-04-13 07:19:31 +0000

    javascript activity files

---
+++ looping-through-arrays.js
@@ -0,0 +1,17 @@
+//var pets = ['cat', 'dog', 'rat'];
+//for (pets[i] = pets[i] + 's');
+//console.log(pets[i]);
+
+//var pets = [‘cat’, ‘dog’, ‘rat’];
+//for (var i = 0; i < pets.length; i++) {
+//pets[i] = pets[i] + ‘s’;
+//console.log(pets[i]);
+//}
+
+let pets = ['cat', 'dog', 'rat'];
+for (pets[i] = pets[i] + 's');
+//for (let i = 0; i < pets.length; i++) { 
+ // pets[i] = pets[i] + 's';
+}
+console.log(pets[i]);
+
